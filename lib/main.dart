ab9f83e1e83d940c1838434bc8fb81afee81c570
XDLOPER <yasirkaygusuzone@hotmail.com>
2023-01-23 04:13:58 +0300

    delete the unnecessary file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'dart:math';
-
 import 'package:createmy_flutter_template/core/init/language_management/language_management.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:extensions/string_extensions/string_extensions.dart';
 import 'package:flutter/material.dart';
 import 'core/constants/app_constants.dart';
 import 'package:test/example/lang_example.dart';
-
-import 'core/init/language_management/locale_keys.g.dart';
 
 Future<void> _init() async {
   WidgetsFlutterBinding.ensureInitialized();
